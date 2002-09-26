680891268d72e60b878b8bcc47147ec4f9c18222
StephanWalter <StephanWalter>
2002-09-26 13:09:14 +0000

    2. Version

--- navigation/navigation1.php
+++ navigation/navigation1.php
@@ -1,16 +1,22 @@
 <?
 class tree{
+	/********** anzupassende Werte ************/
 	var $pfad="c:/php/navigation/";  // Pfad
 	var $db_host="localhost";  // Datenbankhost
 	var $db_user="walter";  // User
 	var $db_passwd="walter";  // Passwort
 	var $db_database="verwaltung"; // Datenbank
+	
 	var $verbindung; // Datenbankverbindung
 	var $knoten_array; // Array aller Knoten
 	var $blatt_array;  // Array aller Blaetter, die zu einem Knoten gehoeren
 	var $knoten;  // Knoten der aktiv ist
 	var $blatt;  // Blatt das aktiv ist
 	
+	var $knoten_schnitt; // an welcher Stelle muss das Knoten-Array geschnitten werden
+	var $blatt_schnitt; // an welcher Stelle muss das Blatt-Array geschnitten werden
+	
+	/*********************** Rueckgabewerte ************************/
 	var $knoten_array_oben_passiv; // Knoten oberhalb passiv
 	var $knoten_block_oben_passiv="hidden"; // Knotenblock passiv oberhalb
 	var $knoten_array_oben_aktiv; // Knoten oberhalb aktiv
@@ -25,6 +31,8 @@ class tree{
 	
 	var $knoten_array_unten_passiv; // Knoten oberhalb passiv
 	var $knoten_block_unten_passiv="hidden"; // Knotenblock passiv oberhalb
+	
+	
 	
 	/**
 	* Konstruktor
@@ -52,53 +60,81 @@ class tree{
 	* Abrufen aller Knoten des Baumes
 	*/
 	function alle_knoten(){
-		$query="select * from knoten1 order by knoten_id";
+		$query="select * from knoten1 order by knoten.knoten_id";
 		$abfrage=mysql_db_query($this->db_database,$query,$this->verbindung);
-		while($arr=mysql_fetch_row($abfrage)){
-			$this->knoten_array[knoten_id][$arr[0]]=$arr[0];
-			$this->knoten_array[knoten_name][$arr[0]]=$arr[1];
-			$this->knoten_array[knoten_pfad][$arr[0]]=$this->pfad.$arr[0]."/".$arr[2];
+		while($arr1=mysql_fetch_array($abfrage,MYSQL_ASSOC)){
+			$this->knoten_array[knoten_id][$arr1[knoten_id]]=$arr1[knoten_id];  // knoten_id
+			$this->knoten_array[knoten_name][$arr1[knoten_id]]=$arr1[knoten_name];  // knoten_name
+			$this->knoten_array[knoten_pfad][$arr1[knoten_id]]=$this->pfad.$arr1[knoten_id]."/".$arr1[knoten_pfad]; // knoten_pfad
+			// $this->kontrolle($arr1);
 		}
-		$this->knoten_zerlegung();
+		
+		// $this->schnittstelle_knoten();
+		// $this->knoten_zerlegung();
+	}
+	
+	/**
+	* Bestimmung der Blaetter die zu einem Knoten gehoeren
+	*/
+	function blaetter_zu_knoten(){
+		$query="select * from knoten1_blatt1,blatt1 where knoten1_blatt1.knoten_id=$this->knoten and knoten1_blatt1.blatt_id=blatt1.id order by blatt1.blatt_id";
+		$abfrage=mysql_db_query('verwaltung',$query,$this->verbindung);
+		while($arr=mysql_fetch_array($abfrage,MYSQL_ASSOC)){
+			$this->blatt_array[blatt_pfad][$arr[blatt_id]]=$this->pfad.$this->knoten."/".$arr[blatt_pfad];  // blatt_pfad
+			$this->blatt_array[blatt_name][$arr[blatt_id]]=$arr[blatt_name];  // blatt_name
+			$this->blatt_array[knoten_id][$arr[blatt_id]]=$arr[knoten_id];  // knoten_id
+			$this->blatt_array[blatt_id][$arr[blatt_id]]=$arr[blatt_id];  // blatt_id
+			// $this->kontrolle($arr);
+		}
 	}
 	
 	/**
 	*	Zerlegung des Knotenarray
 	*/
 	function knoten_zerlegung(){
-		if($this->knoten==false){
-			$this->knoten_array_oben_passiv=$this->knoten_array; // Knoten oberhalb passiv
-			$this->knoten_block_oben_passiv="visibility"; // Knotenblock passiv oberhalb
+		echo "Knotenzerlegung";	
+	}
+	
+	
+	/**
+	* Bestimmung des Knotenschnittpunktes
+	*/
+	function schnittstelle_knoten(){
+		$test1=array_slice($this->knoten_array,0,1);  // Zerlegung in Einzelarray
+		foreach($test1 as $name => $inhalt){
+			$i=0;
+			foreach($inhalt as $name1 => $inhalt1){
+			$anzahl=count($inhalt);
+				if($name1==$knoten){
+					$this->knoten_schnitt=$i;  // Bestimmung Knoten - Schnittstelle
+				}
+				$i++;
+			}
 		}
 	}
 	
 	
 	/**
-	* Bestimmung der Blaetter die zu einem Knoten gehoeren
+	* Kontrollfunktion zur Ueberpruefung eines 2 - dimensionalen Array
 	*/
-	function blaetter_zu_knoten(){
-		$query="select * from knoten1_blatt1,blatt1 where knoten1_blatt1.knoten_id=$this->knoten and knoten1_blatt1.blatt_id=blatt1.id";
-		$abfrage=mysql_db_query('verwaltung',$query,$this->verbindung);
-		while($arr=mysql_fetch_row($abfrage)){
-			$this->blatt_array[blatt_pfad][$arr[0]]=$this->pfad.$this->knoten."/".$arr[5];  // blatt_pfad
-			$this->blatt_array[blatt_name][$arr[0]]=$arr[4];  // blatt_name
-			$this->blatt_array[knoten_id][$arr[0]]=$arr[1];  // knoten_id
-			$this->blatt_array[blatt_id][$arr[0]]=$arr[0];  // blatt_id
+	function kontrolle($test){
+		foreach($test as $name => $inhalt){
+			echo "Name: $name mit Inhalt: $inhalt<br>";
 		}
 	}
 }
 
 
-
+/********************* Ende Klasse ****************/
 
 /**
 * Aufruf des Objektes mit
 *  Knoten = 1
 *  Blatt = 1
 */
-// $knoten=1;
-// $blatt=2;
+$knoten=1;
+$blatt=2;
 
 $baum = new tree($knoten,$blatt);
-echo $baum->knoten_array_oben_passiv;
+echo $baum->knoten_schnitt;
 ?>
--- navigation/test.php
+++ navigation/test.php
@@ -1,29 +1,69 @@
 <?
 /******** Bildung eines mehrdimensionalen Array ***********/
-$test[wert1][0]="wert1 - 0";
 $test[wert1][1]="wert1 - 1";
-$test[wert1][2]="wert1 - 2";
 $test[wert1][3]="wert1 - 3";
-$test[wert1][1]="wert1 - 4";
+$test[wert1][5]="wert1 - 5";
+$test[wert1][7]="wert1 - 7";
+$test[wert1][9]="wert1 - 9";
 
-$test[wert2][0]="wert2 - 0";
 $test[wert2][1]="wert2 - 1";
-$test[wert2][2]="wert2 - 2";
 $test[wert2][3]="wert2 - 3";
-$test[wert2][1]="wert2 - 4";
+$test[wert2][5]="wert2 - 5";
+$test[wert2][7]="wert2 - 7";
+$test[wert2][9]="wert2 - 9";
 
-$test[wert3][0]="wert3 - 0";
 $test[wert3][1]="wert3 - 1";
-$test[wert3][2]="wert3 - 2";
 $test[wert3][3]="wert3 - 3";
-$test[wert3][1]="wert3 - 4";
+$test[wert3][5]="wert3 - 5";
+$test[wert3][7]="wert3 - 7";
+$test[wert3][9]="wert3 - 9";
 
 /******** Steuerungswerte ************/
-$knoten=1;
-$blatt=2;
+$knoten=5;
+$blatt=3;
 
-/********** Zerlegung eines mehrdimensionalen Array ***************/
 
-kommando slice
 
+/****************** Bestimmung Schnittstelle ***************/
+$test1=array_slice($test,0,1);  // Zerlegung in Einzelarray
+foreach($test1 as $name => $inhalt){
+	$i=0;
+	foreach($inhalt as $name1 => $inhalt1){
+	$anzahl=count($inhalt);
+		if($name1==$knoten){
+			$j=$i;  // Bestimmung Schnittstelle
+		}
+		$i++;
+	}
+}
+
+
+/************** Zerlegung Gesamtarray in Teilarrays **************************/
+foreach($test as $name => $inhalt){
+	if($knoten==false and $blatt==false){
+		$oben=$test;
+	}
+	if($knoten==true and $blatt==false){
+		$oben[$name]=array_slice($test[$name],0,$j);
+		$mitte[$name]=array_slice($test[$name],$j,1);
+		$unten[$name]=array_slice($test[$name],$j+1,$anzahl-1);
+	}
+	if($knoten==true and $blatt==true){
+		$oben[$name]=array_slice($test[$name],0,$j+1);
+		$unten[$name]=array_slice($test[$name],$j+1,$anzahl-1);
+	}
+}
+
+
+
+/************* Kontrolle *****************/
+kontrolle($oben);
+
+function kontrolle($test){
+	foreach($test as $name => $inhalt){
+		foreach($inhalt as $name1 => $inhalt1){
+			echo "Name: $name Name1: $name1 Inhalt: $inhalt1<br>";
+		}
+	}
+}
 ?>
